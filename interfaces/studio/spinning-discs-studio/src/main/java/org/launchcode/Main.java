9fdc61c1cd139511d2f3b2c2fd94bac0d4dab6f7
Molly Heininger <heininger.molly@gmail.com>
2023-09-25 12:46:15 -0500

    complete

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -2,9 +2,19 @@ package org.launchcode;
 
 public class Main {
     public static void main(String[] args) {
+        BaseDisc cd = new CD("Fan Mail", 750, "TLC");
+        BaseDisc dvd = new DVD("Gladiator", 1500, "Action");
 
-        // TODO: Declare and initialize a CD and a DVD object.
 
-        // TODO: Call each CD and DVD method to verify that they work as expected.
-    }
+//        System.out.println(dvd.getBasicInfo());
+//
+//        System.out.println(cd.writeData("Don't go chasing waterfalls."));
+//        System.out.println(dvd.writeData("Are you not entertained?"));
+
+        CD newCD = (CD) cd;
+        newCD.spinDisc();
+        System.out.println(newCD.getBasicInfo());
+        newCD.playSong();
+
+        }
 }
